a47d6d677966233a0b60d8c39c17d951678b30e3
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2024-12-27 14:48:40 +0530

    handling newUrlParser error on production by commenting it

--- app/lib/mongodb.js
+++ app/lib/mongodb.js
@@ -4,9 +4,9 @@
 import { MongoClient } from 'mongodb'
 
 const uri = process.env.MONGO_URI
-const options = {
-    useNewUrlParser: true,
-}
+// const options = {
+//     useNewUrlParser: true,
+// }
 
 let client
 let clientPromise
@@ -17,12 +17,12 @@ if (!process.env.MONGO_URI) {
 
 if (process.env.NODE_ENV === 'development') {
     if (!global._mongoClientPromise) {
-        client = new MongoClient(uri, options)
+        client = new MongoClient(uri)
         global._mongoClientPromise = client.connect()
     }
     clientPromise = global._mongoClientPromise
 } else {
-    client = new MongoClient(uri, options)
+    client = new MongoClient(uri)
     clientPromise = client.connect()
 }
 
